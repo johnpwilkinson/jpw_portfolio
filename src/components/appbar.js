ba101a963ba406d8043445e88fcd958ee500bd1b
John Wilkinson <johnpwilkinson@outlook.com>
2020-10-04 19:38:50 -0400

    fixed nav and style

--- src/components/appbar.js
+++ src/components/appbar.js
@@ -14,7 +14,7 @@ export default function Appbar() {
           <Intent to="/projects"><span className="pdf">Projects</span></Intent>
         </li>
         <li className="nav-item mx-4">
-          <a className="nav-link" href={resume}><span className="pdf"><PDF />Download Resume</span></a>
+          <a className="nav-link" ><span className="pdf"><PDF />Download Resume</span></a>
         </li>
       </ul>
     </div>
