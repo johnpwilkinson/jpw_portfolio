071a0588d308bf3c9f10c0b1fc2b01c82cd0a87d
John Wilkinson <johnpwilkinson@outlook.com>
2020-10-04 20:03:00 -0400

    fixed nav and style

--- src/components/appbar.js
+++ src/components/appbar.js
@@ -14,7 +14,7 @@ export default function Appbar() {
           <Intent to="/projects"><span className="pdf">Projects</span></Intent>
         </li>
         <li className="nav-item mx-4">
-          <a href={'./JohnWilkinsonResume.pdf'} className="nav-link" ><span className="pdf"><PDF />Download Resume</span></a>
+          <a href={'../../JohnWilkinsonResume.pdf'} className="nav-link" ><span className="pdf"><PDF />Download Resume</span></a>
         </li>
       </ul>
     </div>
